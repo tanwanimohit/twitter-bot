0b5befe0cde8662f3ff50711ce335b68f674563b
Mohit Tanwani <mohitmurlidhar@gmail.com>
2019-06-02 23:29:55 +0530

    v1 bot is up

---
+++ app.js
@@ -0,0 +1,297 @@
+var config = require('./config.js');
+var Twitter = require('twitter');
+const https = require('https');
+const request = require('request');
+
+var client = new Twitter(config);
+
+var stream = client.stream('statuses/filter', {track: '@BotRaju'});
+stream.on('data', function(event) {
+	
+  //console.log(event.text);
+  //console.log(event);
+   
+	like(event);
+	Reply(event);
+  
+});
+
+ 
+function like(main)
+{
+   var res2= {
+	  Name : "I liked this tweet ",
+	  id:main.id_str,
+   }
+  
+    client.post('favorites/create', res2,
+    function(err, data, response) {
+      //console.log(data);
+    }
+  ); 
+  
+}
+
+function Reply(main)
+{
+  var text=(main.text).toLowerCase();
+  
+  if(text.includes("quote") )
+  {
+	  SendQuote(main);
+  }
+  else if(text.includes("joke") )
+  {
+	  SendJoke(main);
+  }
+  else if(text.includes("-help") )
+  {
+	  Help(main);
+  }
+  else
+  {
+	  let reg = new RegExp("@botraju")
+	  text=text.replace(reg,'');
+	  if(text!=null && text.length>=2) SendDuckDuckGo(main,text);
+	  else Help(main);
+  }
+  
+}
+
+function SendQuote(main)
+{
+	https.get('https://freequote.herokuapp.com/', (resp) => {
+	let data = '';
+
+	  // A chunk of data has been recieved.
+	  resp.on('data', (chunk) => {
+		data += chunk;
+		var res = {
+		status: 'Hello '+main.user.name+', Here is the quote for you : \n'+JSON.parse(data).quote+' - '+JSON.parse(data).author,
+		in_reply_to_status_id:  main.id_str,
+		auto_populate_reply_metadata:true
+		};
+		
+		client.post('statuses/update', res,
+		function(err, data, response) {
+		  console.log(data);
+		}
+		); 
+	  });
+
+	  // The whole response has been received. Print out the result.
+	  resp.on('end', () => {
+		console.log(JSON.parse(data).explanation);
+		
+	  });
+
+	}).on("error", (err) => {
+	  console.log("Error: " + err.message);
+	});
+	
+	
+}
+
+function SendJoke(main)
+{
+	https.get('https://official-joke-api.appspot.com/random_joke', (resp) => {
+	let data = '';
+
+	  // A chunk of data has been recieved.
+	  resp.on('data', (chunk) => {
+		data += chunk;
+		var res = {
+		status: 'Hello '+main.user.name+', Here is the Joke for you : \n'+JSON.parse(data).setup+' \n'+JSON.parse(data).punchline,
+		in_reply_to_status_id:  main.id_str,
+		auto_populate_reply_metadata:true
+		};
+		
+		client.post('statuses/update', res,
+		function(err, data, response) {
+		  console.log(data);
+		}
+		); 
+	  });
+
+	  // The whole response has been received. Print out the result.
+	  resp.on('end', () => {
+		console.log(JSON.parse(data).explanation);
+		
+	  });
+
+	}).on("error", (err) => {
+	  console.log("Error: " + err.message);
+	});
+	
+	
+}
+
+
+
+function SendDuckDuckGo(main,text)
+{
+	
+	try
+	{
+	var url='https://api.duckduckgo.com/?q='+text+'&format=json&pretty=2';
+	console.log(url);
+	
+	request(url, { json: true }, (err, res, data) => {
+	  if (err) { return console.log(err); }
+	  var AbstractURL= data.AbstractURL;
+		var Abstract= data.Abstract;
+		Abstract = Abstract.split(".")[0];
+		if(data.RelatedTopics[0]==null)
+		{
+			var res = {
+			status: 'Hello '+main.user.name+', Sorry ! But I am still Learning, I am not sure about this ! \nTip : Do not Write who,where,whom directly write keywords for example : \nDo not Write "What is RAM", Just Write "RAM"',
+			in_reply_to_status_id:  main.id_str,
+			auto_populate_reply_metadata:true
+			};
+			
+			client.post('statuses/update', res,
+			function(err, data, response) {
+			  console.log(data);
+			}
+			); 
+			
+			return;
+		}
+		var FirstURL=data.RelatedTopics[0].FirstURL;
+		var FirstURLText=data.RelatedTopics[0].Text;
+		FirstURLText = FirstURLText.split(".")[0];
+		
+		if(AbstractURL!="")
+		{
+			if(Abstract!="")
+			{
+				console.log(Abstract);
+				var res = {
+				status: 'Hello '+main.user.name+',\n'+Abstract+' More : '+AbstractURL,
+				in_reply_to_status_id:  main.id_str,
+				auto_populate_reply_metadata:true
+				};
+				
+				client.post('statuses/update', res,
+				function(err, data, response) {
+				  console.log(data);
+				}
+				); 
+			}
+			else if(FirstURLText!="")
+			{
+				console.log("FirstURL Text2"+FirstURLText);
+				var res = {
+				status: 'Hello '+main.user.name+',\n'+FirstURLText+' More : '+AbstractURL,
+				in_reply_to_status_id:  main.id_str,
+				auto_populate_reply_metadata:true,
+				};
+				
+				client.post('statuses/update', res,
+				function(err, data, response) {
+				  console.log(data);
+				}
+				); 
+			}
+			else
+			{
+				var res = {
+				status: 'Hello '+main.user.name+',  Here you Go : '+AbstractURL,
+				in_reply_to_status_id:  main.id_str,
+				auto_populate_reply_metadata:true
+				};
+				
+				client.post('statuses/update', res,
+				function(err, data, response) {
+				  console.log(data);
+				}
+				);
+			}
+			
+		}
+		else if(FirstURL!="")
+		{
+			console.log("FirstURL");
+			if(Abstract!="")
+			{
+				console.log("Abstract2"+Abstract);
+				var res = {
+				status: 'Hello '+main.user.name+',\n'+Abstract+' More : '+FirstURL,
+				in_reply_to_status_id:  main.id_str,
+				auto_populate_reply_metadata:true,
+				media_ids: media.media_id_string
+				};
+				
+				client.post('statuses/update', res,
+				function(err, data, response) {
+				  console.log(data);
+				}
+				); 
+			}
+			else if(FirstURLText!="")
+			{
+				console.log("FirsurlText2"+FirstURLText);
+				var res = {
+				status: 'Hello '+main.user.name+',\n'+FirstURLText+' More : '+FirstURL,
+				in_reply_to_status_id:  main.id_str,
+				auto_populate_reply_metadata:true
+				};
+				
+				client.post('statuses/update', res,
+				function(err, data, response) {
+				  console.log(data);
+				}
+				); 
+			}
+			else
+			{
+				var res = {
+				status: 'Hello '+main.user.name+',  Here you Go : '+FirstURL,
+				in_reply_to_status_id:  main.id_str,
+				auto_populate_reply_metadata:true
+				};
+				
+				client.post('statuses/update', res,
+				function(err, data, response) {
+				  console.log(data);
+				}
+				);
+			}
+		}
+		else
+		{
+			var res = {
+			status: 'Hello '+main.user.name+', Sorry ! But I am still Learning, I am not sure about this ! \nTip : Do not Write who,where,whom directly write keywords for example : \nDo not Write "What is RAM", Just Write "RAM"',
+			in_reply_to_status_id:  main.id_str,
+			auto_populate_reply_metadata:true
+			};
+			
+			client.post('statuses/update', res,
+			function(err, data, response) {
+			  console.log(data);
+			}
+			); 
+		}
+	});
+	}
+	catch(e)
+	{
+		console.log(e);
+	}
+}
+
+function Help(main)
+{
+	var res = {
+	status: 'Hello '+main.user.name+', You can Ask me a Joke, Quote or You can get information about anything... \nTip : Do not Write who,where,whom directly write keywords for example : \nDo not Write "What is RAM", Just Write "RAM"',
+	in_reply_to_status_id:  main.id_str,
+	auto_populate_reply_metadata:true
+	};
+	
+	client.post('statuses/update', res,
+	function(err, data, response) {
+	  console.log(data);
+	}
+	); 
+}
+
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_key_secret,
+  access_token_key: process.env.access_token_key,
+  access_token_secret: process.end.access_token_secret
+}
